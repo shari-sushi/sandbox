795693ced8d70ea30e2e17313ab03ed1af79c856
shari-sushi <susi.imo.niku@gmail.com>
2025-05-19 17:57:08 +0900

    構造化(仮

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
-import Layout from "./components/Layout";
+import Layout from "./pageParts/Layout";
+import PostImagePage from "./pages/PostImagePage/PostImagePage";
 
 function App() {
-  return <Layout>write to play sandbox</Layout>;
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/post-image-page" element={<PostImagePage />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
